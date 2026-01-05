537fd37772d17a21521faaaefb4138d1b88d669a
martijnbentum <bentummartijn@gmail.com>
2026-01-05 17:23:02 +0100

    helper functions to get soxi info

--- audio.py
+++ audio.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from progressbar import progressbar
 import math
 import soundfile as sf
+import subprocess
 
 
 def load_audio(filename, sr=None, mono=True):
@@ -64,4 +65,27 @@ def find_all_zero_crossings(y, start_index = 0, end_index = None,
         m += f"{perc_signal_after_last:.2f}%"
         print(m)
     return zero_crossings
+
+def audio_info(filename):
+    info = soxi_info(filename)
+    return soxinfo_to_dict(info)
+
+def soxi_info(filename):
+    o = subprocess.run(['sox','--i',filename],stdout=subprocess.PIPE)
+    return o.stdout.decode('utf-8')
+
+def clock_to_duration_in_seconds(t):
+    hours, minutes, seconds = t.split(':')
+    s = float(hours) * 3600 + float(minutes) * 60 + float(seconds)
+    return s
+
+def soxinfo_to_dict(soxinfo):
+    x = soxinfo.split('\n')
+    d = {}
+    d['filename'] = x[1].split(': ')[-1].strip("'")
+    d['n_channels'] = int(x[2].split(': ')[-1])
+    d['sample_rate'] = int(x[3].split(': ')[-1])
+    t = x[5].split(': ')[-1].split(' =')[0]
+    d['duration'] = clock_to_duration_in_seconds(t)
+    return d
         
